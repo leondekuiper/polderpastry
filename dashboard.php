7433f40fe173c3a6961de5a4f729b6826c761df4
leondekuiper <leondekuiper@gmail.com>
2018-12-15 01:17:47 +0100

    test

--- dashboard.php
+++ dashboard.php
@@ -3,7 +3,7 @@
 $page = 'dashboard'; 
 $title = 'dashboard';
 $content = '<a href="" class=message-small>Foto upload</a></br>
-<a href="" class="message-small">Assortiment Overview</a></br>
+<a href="itemoverview.php" class="message-small">Assortiment Overview</a></br>
 <a href="" class="message-small">Label Overview</a></br>        
 <a href="" class="message-small">Event Overview</a></br>
 <a href="" class="message-small">User Overview</a></br></br</br>';
