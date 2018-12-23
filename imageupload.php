3667d984f934bbd10f1c37b5d91cea0523d5bcd4
leondekuiper <leondekuiper@gmail.com>
2018-12-23 13:58:27 +0100

    test

--- imageupload.php
+++ imageupload.php
@@ -2,4 +2,33 @@
 
 require 'Controller/ImageController.php';
 
+$title = "Upload new image";
+$page = "imageupload";
+        
+$content = '<a href="itemoverview.php" class=message-small>back to Overview</a></br></br>
+    <form action="" method="post" enctype="multipart/form-data">
+        <label for="file">Filename: </label>
+    <input type="file" name="file" id="file" class="upload-button">
+    <input type="submit" name="submit" value="upload"></br></br>
+</form>';
+if(isset($_POST["submit"]))
+{
+    $filetype = $_FILES["file"]["type"];
+
+    if(($filetype == "image/gif") ||
+       ($filetype == "image/jpg") ||
+       ($filetype == "image/jpeg") ||
+       ($filetype == "image/png"))
+    {
+        if(file_exists("Images/" . $_FILES["file"]["name"]))
+        {
+            echo "File already exists";
+        }
+        else
+        {
+            move_uploaded_file($_FILES["file"]["tmp name"], "Images/" . $_FILES["file"]["name"]);
+            echo "File Uploaded";
+        }
+    }
+}
 include 'template_admin.php';
